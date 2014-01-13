1cb3b7236ce3edc5d028b1f6f757a08d902f86c5
imaguiraga <imaguiraga@gmail.com>
2014-01-12 21:40:42 -0500

    typescript

--- experiments/canvas/webapp/scripts/js/main.js
+++ experiments/canvas/webapp/scripts/js/main.js
@@ -1,319 +1,199 @@
-$(document).ready(function() {
-	var client, destination;
-    if(window.WebSocket) {
+/*
+* ...
+* @author
+*/
+var Main = (function () {
+    function Main() {
+    }
+    Main.main = function () {
+        var cScanView = new CScanView("tutorial");
+        $("#bitmap-slider-range").slider({
+            range: true,
+            min: 0,
+            max: 511,
+            values: [0, 511],
+            slide: function (event, ui) {
+                $("#bitmap-range").val(ui.values[0] + " - " + ui.values[1]);
+                cScanView.updateRange(ui.values[0], ui.values[1], cScanView.buffer);
+            }
+        });
 
-      $('#connect_form').submit(function() {
-        var url = $("#connect_url").val();
-        var login = $("#connect_login").val();
-        var passcode = $("#connect_passcode").val();
-        destination = $("#destination").val();
+        $("#bitmap-range").val($("#bitmap-slider-range").slider("values", 0) + " - " + $("#bitmap-slider-range").slider("values", 1));
+    };
+    return Main;
+})();
 
-        client = Stomp.client(url);
-        client.heartbeat.outgoing = 20000; // client will send heartbeats every 20000ms
-        client.heartbeat.incoming = 0;     // client does not want to receive heartbeats
-                                           // from the server
-        // this allows to display debug logs directly on the web page
-        client.debug = function(str) {
-          var log = $("#debug");
-          log.append(str + "\n");
-          log[0].scrollTop = log[0].scrollHeight - log[0].clientHeight;
+var CScanView = (function () {
+    function CScanView(id) {
+        console.log("this = " + this);
+        var scan = this;
+        this.palette = [];
+        this.canvas = document.getElementById(id);
+
+        this.layover = this.canvas.cloneNode();
+        this.canvas.parentNode.appendChild(this.layover);
+        this.layover.onmousemove = function (evt) {
+            var rect = this.getBoundingClientRect();
+            var x0 = evt.x - rect.left;
+            var y0 = evt.y - rect.top;
+            if (this.getContext) {
+                var ctx = this.getContext("2d");
+
+                //clear drawing area
+                ctx.save();
+                ctx.clearRect(0, 0, this.width, this.height);
+                ctx.beginPath();
+                ctx.strokeStyle = "black";
+                ctx.lineWidth = 1;
+                ctx.moveTo(0, y0);
+                ctx.lineTo(this.width, y0);
+                ctx.moveTo(x0, 0);
+                ctx.lineTo(x0, this.height);
+                ctx.stroke();
+                ctx.restore();
+            }
         };
 
-        // the client is notified when it is connected to the server.
-		client.connect(login, passcode, function(frame) {
-		      client.debug("connected to Stomp");
-			  $('#connect').fadeOut({ duration: 'fast' });
-			  $('#connected').fadeIn();
+        var SIZE = this.canvas.width * this.canvas.height;
 
-			  client.subscribe("/topic/InvokeWorkflowOut", function(message) {
-				  $("#messages").html("<p>" + message.body + "</p>\n");
-		      });
+        this.buffer = CScanView.initialiseBuffer(SIZE);
+        this.buffer = CScanView.createRandomSquares(this.buffer, this.canvas.width, this.canvas.height);
 
-			  client.subscribe("/topic/InvokeWorkflowCmdsOut", function(message) {
-				  $("#messages").html("<p>" + JSON.stringify(JSON.parse(message.body), null, 4) + "</p>\n");
-		      });
-
-			  /*
-			  client.subscribe("/topic/InvokeShellCmdsOut", function(message) {
-				  $("#messages").html("<p>" + JSON.stringify(JSON.parse(message.body), null, 4) + "</p>\n");
-		      });
-			  //*/
-
-		});
-        return false;
-      });
-
-	  function builCommandOperation(operation,targetId,methodName,data){
-		var body = {
-				"operation":operation,
-				"targetId":targetId,
-				"methodName":methodName,
-				"params":{
-					"data":data,
-					"len": data.length,
-					"count":1
-				}
-			};
-		return body;
-	  }
-
-	  function builCommandMessage(operation,targetId,methodName,data ){
-			var body = builCommandOperation(operation,targetId,methodName,data);
-
-			var input = {
-				"frame": "SEND",
-				"headers":{
-					"from": "from",
-		  			"to" : "command",
-		  			"correlationId":"correlationId"
-				},
-				"operations":[
-				     body
-				]
-			};
-
-			return JSON.stringify(input);
-	  }
-
-      $('#disconnect').click(function() {
-        client.disconnect(function() {
-          $('#connected').fadeOut({ duration: 'fast' });
-          $('#connect').fadeIn();
-          $("#messages").html("");
-          $("#debug").html("");
-        });
-        return false;
-      });
-
-      $('#btn-send').click(function() {
-          var text = $('#send_form_input').val();
-          if (text) {
-            client.send(destination, {}, text);
-            $('#send_form_input').val("");
-          }
-          return false;
-        });
-
-	  	$("#btn-send-cmd").click(function(evt){
-
-	  		var data = $("#send_form_input").val().trim();
-	  		var input = builCommandMessage("call",this.id,"send",data);
-
-	  		client.send("/queue/InvokeWorkflowCmdsIn", {}, JSON.stringify(input, null, 4));
-	  	});
-
-    } else {
-      $("#connect").html("\
-          <h1>Get a new Web Browser!</h1>\
-          <p>\
-          Your browser does not support WebSockets. This example will not work properly.<br>\
-          Please use a Web Browser with WebSockets support (WebKit or Google Chrome).\
-          </p>\
-      ");
+        this.palette = CScanView.palette();
+        this.draw();
     }
-   /*
-  });
+    CScanView.palette = function () {
+        var canvas = document.createElement("canvas");
+        canvas.width = 512;
+        canvas.height = 1;
+        var palette = [];
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
 
-jQuery(document).ready(function($) {
-	//*/
+            // Create gradient
+            var grd = ctx.createLinearGradient(0, 0, canvas.width, 0);
+            grd.addColorStop(0, "blue");
+            grd.addColorStop(0.5, "yellow");
+            grd.addColorStop(1, "red");
 
+            // Fill with gradient
+            ctx.fillStyle = grd;
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-    var id = 1;
-    var shellinput = [];
-    var shellSocket = null;
-    var terminal = null;
+            //create palette
+            var data = imgData.data;
 
-    function connectTerminal(term){
-    	var PORT =  "53633" || APP_PORT ;
-    	terminal = term;
-    	if(shellSocket != null){
-    		disconnectTerminal(term);
-    	}
-
-		shellSocket = new  WebSocket("ws://localhost:"+PORT+"/shell", ['soap', 'xmpp']);
-
-		shellSocket.onopen = function (evt) {
-			console.log('Command WebSocket Connected');
-			term.echo('Command WebSocket Connected',{
-                finalize: function(div) {
-                    div.css("color", "green");
-                }
-            });
-		};
-
-		shellSocket.onclose = function (evt) {
-			console.log('Command WebSocket Closed');
-			term.echo('Command WebSocket Closed',{
-                finalize: function(div) {
-                    div.css("color", "red");
-                }
-            });
-		};
-
-		// Log errors
-		shellSocket.onerror = function (error) {
-			console.log('Command WebSocket Error ' + error);
-			term.echo('Command WebSocket Error ' + error,{
-                finalize: function(div) {
-                    div.css("color", "red");
-                }
-            });
-		};
-
-		// Log messages from the server
-		shellSocket.onmessage = function (e) {
-			console.log('Server: ' + e.data);
-			var result = e.data;
-			try{
-				 result = JSON.parse(e.data);
-			}catch(e){
-				result = e.data;
-			}
-
-			if(result.hasOwnProperty("operations")){
-				if(result.operations.length && result.operations.length>0 ){
-					result = result.operations[0].params.data;
-					try{
-						result = JSON.stringify(JSON.parse(result),null,4);
-					}catch(e){
-						result = JSON.stringify(result,null,4);
-					}
-				}
-
-			}else{
-				result = String(e.data);
-			}
-
-			term.echo(result,{
-                finalize: function(div) {
-                    div.css("color", "blue");
-                }
-            });
-		};
-    }
-
-    function disconnectTerminal(term){
-    	if(shellSocket != null){
-    		shellSocket.close();
-    	}
-    	shellSocket = null;
-    }
-
-    function islocal(command,term){
-    	var result = false;
-    	if (command == 'help') {
-            term.echo("available commands are <open>, <close>, <commands>, <bindings>, <help> use 'go' at the end of scripts",{
-                finalize: function(div) {
-                    div.css("color", "green");
-                }
-            });
-            result = true;
-
-        }else if(command == "open"){
-			connectTerminal(term);
-			shellinput = [];
-			result = true;
-
-		}else if(command == "close"){
-			disconnectTerminal(term);
-			shellinput = [];
-			result = true;
-		}
-    	return result;
-    }
-
-    $('#console-pane').terminal(function(command, term) {
-    	//trim command
-
-        if (command == 'help') {
-            term.echo("available commands are <open>, <help>",{
-                finalize: function(div) {
-                    div.css("color", "green");
-                }
-            });
-
-        } else if (command == "open") {
-        	connectTerminal(term);
-            term.push(
-            	function(command, term) {
-            		command = command.trim();
-                	var n = command.lastIndexOf("go");
-            		if (islocal(command,term)) {
-
-            		}else if(command === "commands" || command === "bindings" || command === "metrics"){
-		            	term.pause();
-		            	if(shellSocket != null){
-		            		var message = builCommandMessage("call","shell","execute",command);
-		            		shellSocket.send(message);
-            			}else{
-            				term.echo('socket is closed please "connect" first',{
-            	                finalize: function(div) {
-            	                    div.css("color", "red");
-            	                }
-            	            });
-            			}
-		                term.resume();
-		                //reset buffer
-		                //shellinput = [];
-
-            		}else if(command == "go"){
-            			if(shellinput.length >0){
-	            			command = shellinput.join("\n");
-	            			if (!islocal(command,term)){
-				            	term.pause();
-				            	if(shellSocket != null){
-				            		var message = builCommandMessage("call","shell","execute",command);
-				            		shellSocket.send(message);
-		            			}else{
-		            				term.echo('socket is closed please "connect" first',{
-		            	                finalize: function(div) {
-		            	                    div.css("color", "red");
-		            	                }
-		            	            });
-		            			}
-				                term.resume();
-				                //reset buffer
-				                shellinput = [];
-	            			}
-            			}
-
-            		}else if(n>-1){//ends with go
-            			command = command.substring(0,n).trim();
-            			if(command.length >0){
-	            			if (!islocal(command,term)){
-				            	term.pause();
-				            	if(shellSocket != null){
-				            		var message = builCommandMessage("call","shell","execute",command);
-				            		shellSocket.send(message);
-		            			}else{
-		            				term.echo('socket is closed please "connect" first',{
-		            	                finalize: function(div) {
-		            	                    div.css("color", "red");
-		            	                }
-		            	            });
-		            			}
-				                term.resume();
-				                //reset buffer
-				                shellinput = [];
-	            			}
-            			}
-
-            		}else{
-            			shellinput.push(command);
-            		}
-	            }, {
-	                name: 'groovy',
-	                prompt: 'groovy> '
-	            }
-            );
-
-        }  else {
-            term.echo("unknown command " + command);
+            for (var i = 0; i < canvas.width; i++) {
+                var offset = 4 * i;
+                palette.push({
+                    value: [
+                        data[offset + 0],
+                        data[offset + 1],
+                        data[offset + 2],
+                        data[offset + 3]
+                    ]
+                });
+            }
+            console.log("palette=" + palette.length);
         }
+        return palette;
+    };
 
-    }, {
-        greetings: "terminals demo use help to see available commands"
-    });
+    CScanView.createRandomSquares = function (buffer, width, height) {
+        var view = new Uint16Array(buffer);
 
+        for (var i = 0; i < 30; i++) {
+            var x0 = Math.floor(Math.random() * (width - 20));
+            var y0 = Math.floor(Math.random() * (height - 20));
+            var value = Math.floor(Math.random() * 10);
+            value = Math.floor(511 / value);
+            value = Math.floor(Math.random() * 512);
 
+            x0 = x0 + y0 * width;
+
+            for (var r = 0; r < 20; r++) {
+                for (var c = 0; c < 30; c++) {
+                    var index = r * width + x0 + c;
+                    view[index] = value;
+                }
+            }
+        }
+        return buffer;
+    };
+
+    CScanView.initialiseBuffer = function (size) {
+        var buffer = new ArrayBuffer(size * Uint16Array.BYTES_PER_ELEMENT);
+        var view = new Uint16Array(buffer);
+
+        for (var i = 0; i < size; i++) {
+            var value = Math.floor(Math.random() * 511);
+            view[i] = value;
+        }
+        return buffer;
+    };
+
+    CScanView.prototype.updateRange = function (min, max, buffer) {
+        var canvas = this.canvas;
+        var view = new Uint16Array(this.buffer);
+
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            var data = imgData.data;
+
+            for (var i = 0; i < view.length; i++) {
+                var offset = 4 * i;
+
+                //lookup palette value
+                var value = view[i];
+                var color = this.palette[value];
+                data[offset + 0] = color.value[0];
+                data[offset + 1] = color.value[1];
+                data[offset + 2] = color.value[2];
+                data[offset + 3] = color.value[3];
+
+                if (value < min || value > max) {
+                    data[offset + 3] = 0;
+                }
+            }
+            ctx.putImageData(imgData, 0, 0);
+        }
+    };
+
+    CScanView.prototype.draw = function () {
+        var canvas = this.canvas;
+        var view = new Uint16Array(this.buffer);
+
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+            //create palette
+            var data = imgData.data;
+            var length = view.length;
+            console.log(" length=" + length);
+            for (var i = 0; i < length; i++) {
+                var offset = 4 * i;
+
+                //lookup palette value
+                var value = view[i];
+                var color = this.palette[value];
+                if (typeof color === "undefined") {
+                    console.log(i + " color=" + value);
+                }
+                data[offset + 0] = color.value[0];
+                data[offset + 1] = color.value[1];
+                data[offset + 2] = color.value[2];
+                data[offset + 3] = color.value[3];
+            }
+            ctx.putImageData(imgData, 0, 0);
+            return imgData;
+        }
+    };
+    return CScanView;
+})();
+
+$(document).ready(function () {
+    Main.main();
 });
---
+++ experiments/canvas/webapp/ts/Main.ts
@@ -0,0 +1,202 @@
+/* 
+ * ...
+ * @author 
+ */
+class Main {
+	
+	static main():void 	{
+		var cScanView = new CScanView("tutorial");
+		$( "#bitmap-slider-range" ).slider({
+			range: true,
+			min: 0,
+			max: 511,
+			values: [ 0, 511 ],
+			slide: function( event, ui ) {
+				$( "#bitmap-range" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+				cScanView.updateRange(ui.values[ 0 ],ui.values[ 1 ],cScanView.buffer);
+			}
+		});
+
+		$( "#bitmap-range" ).val( 
+			$( "#bitmap-slider-range" ).slider( "values", 0 ) +	" - " + $( "#bitmap-slider-range" ).slider( "values", 1 )
+		);
+	}
+	
+}
+
+class CScanView{
+	palette:any;
+	canvas:any;
+	layover:any;
+	
+	constructor(id:string){
+		console.log("this = "+this);
+		var scan = this;
+		this.palette = [];
+		this.canvas = document.getElementById(id);
+
+		this.layover = this.canvas.cloneNode();
+		this.canvas.parentNode.appendChild(this.layover);
+		this.layover.onmousemove = function(evt){
+			var rect = this.getBoundingClientRect();
+			var  x0 = evt.x - rect.left;
+			var  y0 = evt.y - rect.top;
+			if (this.getContext){
+				var  ctx  = this.getContext("2d");
+				//clear drawing area
+				ctx.save();
+				ctx.clearRect(0,0,this.width,this.height);
+				ctx.beginPath();
+				ctx.strokeStyle="black";
+				ctx.lineWidth=1;
+				ctx.moveTo(0,y0);
+				ctx.lineTo(this.width,y0);
+				ctx.moveTo(x0,0);
+				ctx.lineTo(x0,this.height);
+				ctx.stroke();
+				ctx.restore();
+			}
+		};
+		   
+		var SIZE = this.canvas.width*this.canvas.height;
+
+		this.buffer = CScanView.initialiseBuffer(SIZE);
+		this.buffer = CScanView.createRandomSquares(this.buffer,this.canvas.width,this.canvas.height);
+		
+		this.palette = CScanView.palette();
+		this.draw();
+	}
+			
+	static palette(){
+		var canvas = document.createElement("canvas");
+		canvas.width=512;
+		canvas.height = 1;
+		var palette = [] ;
+		if (canvas.getContext){
+			var ctx = canvas.getContext('2d');
+			// Create gradient
+			var grd=ctx.createLinearGradient(0,0,canvas.width,0);
+			grd.addColorStop(0,"blue");
+			grd.addColorStop(0.5,"yellow");
+			grd.addColorStop(1,"red");
+
+			// Fill with gradient
+			ctx.fillStyle=grd;
+			ctx.fillRect(0,0,canvas.width,canvas.height);
+			var imgData=ctx.getImageData(0,0,canvas.width,canvas.height);
+			//create palette
+
+			var data = imgData.data;
+			//use the first line to extract the color palette
+			for(var i=0; i< canvas.width;i++){
+				var offset = 4*i;
+				palette.push(
+				  { value:[
+					  data[offset+0],data[offset+1],data[offset+2],data[offset+3]
+					]
+				  }
+
+				);
+			}
+			console.log("palette="+palette.length);
+		}
+		return palette;
+	}
+	
+	static createRandomSquares(buffer,width,height){
+		var view = new Uint16Array(buffer);
+
+		for(var i = 0;i < 30;i++){
+			var x0 = Math.floor(Math.random()*(width-20));
+			var y0 = Math.floor(Math.random()*(height-20));
+			var value = Math.floor(Math.random()*10);
+			value = Math.floor(511/value);
+			value = Math.floor(Math.random()*512);
+
+			x0 = x0+y0*width;
+
+			for (var r = 0; r < 20; r++) {
+				for (var c = 0; c < 30; c++) {
+					var index = r*width+x0+c;
+					view[index] = value;
+				}
+			}
+		}
+		return buffer;
+
+	}
+	
+	static initialiseBuffer(size){
+		var buffer = new ArrayBuffer(size*Uint16Array.BYTES_PER_ELEMENT);
+		var view = new Uint16Array(buffer);
+
+		for(var i = 0;i < size;i++){
+			var value = Math.floor(Math.random()*511);
+			view[i] = value;
+		}
+		return buffer;
+	}
+	
+	updateRange(min,max,buffer){
+		var canvas = this.canvas;
+		var view = new Uint16Array(this.buffer);
+
+		if (canvas.getContext){
+			var ctx = canvas.getContext('2d');
+			var imgData=ctx.getImageData(0,0,canvas.width,canvas.height);
+			var data = imgData.data;
+
+			for(var i=0; i< view.length; i++){
+				var offset = 4*i;
+				//lookup palette value
+				var value = view[i];
+				var color = this.palette[value];
+				data[offset+0] = color.value[0];
+				data[offset+1] = color.value[1];
+				data[offset+2] = color.value[2];
+				data[offset+3] = color.value[3];
+
+				if(value < min || value > max){
+					data[offset+3] = 0;
+				}
+
+			}
+			ctx.putImageData(imgData,0,0);
+		}
+	}
+		
+	draw(){
+		var canvas = this.canvas;
+		var view = new Uint16Array(this.buffer);
+
+		if (canvas.getContext){
+			var ctx = canvas.getContext('2d');
+			var imgData=ctx.getImageData(0,0,canvas.width,canvas.height);
+			//create palette
+
+			var data = imgData.data;
+			var length = view.length;
+			console.log(" length="+length);
+			for(var i=0; i< length; i++){
+				var offset = 4*i;
+				//lookup palette value
+				var value = view[i];
+				var color = this.palette[value];
+				if( typeof color === "undefined"){
+					console.log(i+" color="+value);
+				}
+				data[offset+0] = color.value[0];
+				data[offset+1] = color.value[1];
+				data[offset+2] = color.value[2];
+				data[offset+3] = color.value[3];
+
+			}
+			ctx.putImageData(imgData,0,0);
+			return imgData;
+		}
+	}
+}
+
+$(document).ready(function() {
+	Main.main();
+});
