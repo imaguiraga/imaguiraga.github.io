0f6a6315a90a0d2a3eb5e7a36d8a6416d8cb86c3
imaguiraga <imaguiraga@gmail.com>
2014-03-03 00:23:40 -0500

    update

--- experiments/fx-selector/webapp/fxData2.js
+++ experiments/fx-selector/webapp/fxData2.js
@@ -1,5 +1,171 @@
+function fxServer(){
+ var root = 
+	{
+	  "type": "serverbox",
+	  "name": "serverbox",
+	  "descr": "serverbox",
+	  "icon": "fa-serverbox",
+	  "entries": [
+		{
+		  "type": "windows",
+		  "name": "windows",
+		  "descr": "windows",
+		  "icon": "fa-windows",
+		  "entries": []
+		},
+		{
+		  "type": "linux",
+		  "name": "linux",
+		  "descr": "linux",
+		  "icon": "fa-linux",
+		  "entries": []
+		}
+	  ]
+	};return root;
+}
+	
+function fxSmartTV(){
+	var root = 
+	{
+	  "type": "hdtv",
+	  "name": "hdtv",
+	  "descr": "hdtv",
+	  "icon": "fa-hdtv",
+	  "entries": [
+		{
+		  "type": "tizen",
+		  "name": "tizen",
+		  "descr": "tizen",
+		  "icon": "fa-tizen",
+		  "entries": []
+		},
+		{
+		  "type": "samsung",
+		  "name": "samsung",
+		  "descr": "samsung",
+		  "icon": "fa-samsung",
+		  "entries": []
+		}
+	  ]
+	};return root;
+}
+
+function fxMobile(){
+	var root= 
+	{
+	  "type": "mobile",
+	  "name": "mobile",
+	  "descr": "mobile",
+	  "icon": "fa-mobile",
+	  "entries": [
+		{
+		  "type": "android",
+		  "name": "android",
+		  "descr": "android",
+		  "icon": "fa-android",
+		  "entries": []
+		},
+		{
+		  "type": "ios",
+		  "name": "ios",
+		  "descr": "ios",
+		  "icon": "fa-ios",
+		  "entries": []
+		},
+		{
+		  "type": "winphone",
+		  "name": "winphone",
+		  "descr": "winphone",
+		  "icon": "fa-winphone",
+		  "entries": []
+		},
+		{
+		  "type": "blackberry",
+		  "name": "blackberry",
+		  "descr": "blackberry",
+		  "icon": "fa-blackberry",
+		  "entries": []
+		}
+	  ]
+	};
+	return root;
+}
+
+function fxDesktop(){
+	var root = 
+	{
+		"type": "desktop",
+		"name": "desktop",
+		"descr": "desktop",
+		"icon": "fa-desktop",
+		"entries": [
+			{
+			  "type": "mac",
+			  "name": "mac",
+			  "descr": "mac",
+			  "icon": "fa-mac",
+			  "entries": []
+			},
+			{
+			  "type": "windows",
+			  "name": "windows",
+			  "descr": "windows",
+			  "icon": "fa-windows",
+			  "entries": []
+			},
+			{
+			  "type": "linux",
+			  "name": "linux",
+			  "descr": "linux",
+			  "icon": "fa-linux",
+			  "entries": []
+			}
+		]
+	};return root;
+}
+
+function fxTablet(){
+	var root =  
+	{
+	  "type": "tablet",
+	  "name": "tablet",
+	  "descr": "tablet",
+	  "icon": "fa-tablet",
+	  "entries": [
+		{
+		  "type": "android",
+		  "name": "android",
+		  "descr": "android",
+		  "icon": "fa-android",
+		  "entries": []
+		},
+		{
+		  "type": "ios",
+		  "name": "ios",
+		  "descr": "ios",
+		  "icon": "fa-ios",
+		  "entries": []
+		},
+		{
+		  "type": "windows",
+		  "name": "windows",
+		  "descr": "windows",
+		  "icon": "fa-windows",
+		  "entries": []
+		},
+		{
+		  "type": "blackberry",
+		  "name": "blackberry",
+		  "descr": "blackberry",
+		  "icon": "fa-blackberry",
+		  "entries": []
+		}
+	  ]
+	};return root;
+}
+
 function fxData2(){
-var root=
+var root =
 {
   "type": "root",
   "name": "users",
@@ -30,129 +196,11 @@ var root=
                   "descr": "device",
                   "icon": "fa-device",
                   "entries": [
-                    {
-                      "type": "mobile",
-                      "name": "mobile",
-                      "descr": "mobile",
-                      "icon": "fa-mobile",
-                       "entries": [
-                            {
-                              "type": "android",
-                              "name": "android",
-                              "descr": "android",
-                              "icon": "fa-android",
-                              "entries": []
-                            },
-                            {
-                              "type": "ios",
-                              "name": "ios",
-                              "descr": "ios",
-                              "icon": "fa-ios",
-                              "entries": []
-                            },
-                            {
-                              "type": "winphone",
-                              "name": "winphone",
-                              "descr": "winphone",
-                              "icon": "fa-winphone",
-                              "entries": []
-                            },
-                            {
-                              "type": "blackberry",
-                              "name": "blackberry",
-                              "descr": "blackberry",
-                              "icon": "fa-blackberry",
-                              "entries": []
-                            }
-                          ]
-                    },
-                    {
-                      "type": "desktop",
-                      "name": "desktop",
-                      "descr": "desktop",
-                      "icon": "fa-desktop",
-                      "entries": [
-                            {
-                              "type": "mac",
-                              "name": "mac",
-                              "descr": "mac",
-                              "icon": "fa-mac",
-                              "entries": []
-                            },
-                            {
-                              "type": "windows",
-                              "name": "windows",
-                              "descr": "windows",
-                              "icon": "fa-windows",
-                              "entries": []
-                            },
-                            {
-                              "type": "linux",
-                              "name": "linux",
-                              "descr": "linux",
-                              "icon": "fa-linux",
-                              "entries": []
-                            }
-                          ]
-                    },
-                    {
-                      "type": "tablet",
-                      "name": "tablet",
-                      "descr": "tablet",
-                      "icon": "fa-tablet",
-                     "entries": [
-                            {
-                              "type": "android",
-                              "name": "android",
-                              "descr": "android",
-                              "icon": "fa-android",
-                              "entries": []
-                            },
-                            {
-                              "type": "ios",
-                              "name": "ios",
-                              "descr": "ios",
-                              "icon": "fa-ios",
-                              "entries": []
-                            },
-                            {
-                              "type": "windows",
-                              "name": "windows",
-                              "descr": "windows",
-                              "icon": "fa-windows",
-                              "entries": []
-                            },
-                            {
-                              "type": "blackberry",
-                              "name": "blackberry",
-                              "descr": "blackberry",
-                              "icon": "fa-blackberry",
-                              "entries": []
-                            }
-                          ]
-                    },
-                    {
-                      "type": "hdtv",
-                      "name": "hdtv",
-                      "descr": "hdtv",
-                      "icon": "fa-hdtv",
-                      "entries": [
-                            {
-                              "type": "tizen",
-                              "name": "tizen",
-                              "descr": "tizen",
-                              "icon": "fa-tizen",
-                              "entries": []
-                            },
-                            {
-                              "type": "samsung",
-                              "name": "samsung",
-                              "descr": "samsung",
-                              "icon": "fa-samsung",
-                              "entries": []
-                            }
-                          ]
-                    }
+                    fxMobile(),
+                    fxDesktop(),
+                    fxTablet(),
+					fxSmartTV()
+                   
                   ]
                 }
               ]
@@ -185,129 +233,10 @@ var root=
                   "descr": "device",
                   "icon": "fa-device",
                   "entries": [
-                    {
-                      "type": "mobile",
-                      "name": "mobile",
-                      "descr": "mobile",
-                      "icon": "fa-mobile",
-                       "entries": [
-                            {
-                              "type": "android",
-                              "name": "android",
-                              "descr": "android",
-                              "icon": "fa-android",
-                              "entries": []
-                            },
-                            {
-                              "type": "ios",
-                              "name": "ios",
-                              "descr": "ios",
-                              "icon": "fa-ios",
-                              "entries": []
-                            },
-                            {
-                              "type": "winphone",
-                              "name": "winphone",
-                              "descr": "winphone",
-                              "icon": "fa-winphone",
-                              "entries": []
-                            },
-                            {
-                              "type": "blackberry",
-                              "name": "blackberry",
-                              "descr": "blackberry",
-                              "icon": "fa-blackberry",
-                              "entries": []
-                            }
-                          ]
-                    },
-                    {
-                      "type": "desktop",
-                      "name": "desktop",
-                      "descr": "desktop",
-                      "icon": "fa-desktop",
-                      "entries": [
-                            {
-                              "type": "mac",
-                              "name": "mac",
-                              "descr": "mac",
-                              "icon": "fa-mac",
-                              "entries": []
-                            },
-                            {
-                              "type": "windows",
-                              "name": "windows",
-                              "descr": "windows",
-                              "icon": "fa-windows",
-                              "entries": []
-                            },
-                            {
-                              "type": "linux",
-                              "name": "linux",
-                              "descr": "linux",
-                              "icon": "fa-linux",
-                              "entries": []
-                            }
-                          ]
-                    },
-                    {
-                      "type": "tablet",
-                      "name": "tablet",
-                      "descr": "tablet",
-                      "icon": "fa-tablet",
-                      "entries": [
-                            {
-                              "type": "android",
-                              "name": "android",
-                              "descr": "android",
-                              "icon": "fa-android",
-                              "entries": []
-                            },
-                            {
-                              "type": "ios",
-                              "name": "ios",
-                              "descr": "ios",
-                              "icon": "fa-ios",
-                              "entries": []
-                            },
-                            {
-                              "type": "windows",
-                              "name": "windows",
-                              "descr": "windows",
-                              "icon": "fa-windows",
-                              "entries": []
-                            },
-                            {
-                              "type": "blackberry",
-                              "name": "blackberry",
-                              "descr": "blackberry",
-                              "icon": "fa-blackberry",
-                              "entries": []
-                            }
-                          ]
-                    },
-                    {
-                      "type": "hdtv",
-                      "name": "hdtv",
-                      "descr": "hdtv",
-                      "icon": "fa-hdtv",
-                      "entries": [
-                                {
-                                  "type": "tizen",
-                                  "name": "tizen",
-                                  "descr": "tizen",
-                                  "icon": "fa-tizen",
-                                  "entries": []
-                                },
-                                {
-                                  "type": "samsung",
-                                  "name": "samsung",
-                                  "descr": "samsung",
-                                  "icon": "fa-samsung",
-                                  "entries": []
-                                }
-                              ]
-                    }
+                    fxMobile(),
+                    fxDesktop(),
+                    fxTablet(),
+                    fxSmartTV()
                   ]
                 }
               ]
@@ -318,33 +247,8 @@ var root=
               "descr": "server",
               "icon": "fa-server",
               "entries": [
-                
-                    {
-                      "type": "serverbox",
-                      "name": "serverbox",
-                      "descr": "serverbox",
-                      "icon": "fa-serverbox",
-                      
-                          "entries": [
-                            {
-                              "type": "windows",
-                              "name": "windows",
-                              "descr": "windows",
-                              "icon": "fa-windows",
-                              "entries": []
-                            },
-                            {
-                              "type": "linux",
-                              "name": "linux",
-                              "descr": "linux",
-                              "icon": "fa-linux",
-                              "entries": []
-                            }
-                          ]
-                      
-                    }
-                  ]
-              
+                fxServer()
+              ]
             }
           ]
         }
